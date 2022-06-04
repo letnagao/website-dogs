959f96d71e321bf0e5db499c2e64ac3f2ae339c4
˜Lumi <lu.nagao021@gmail.com>
2022-06-04 01:09:59 -0300

    setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
 import './App.css';
+import Header from './Componentes/Header'; 
+import Footer from './Componentes/Footer';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          
+        </Routes>
+        <Footer />      
+      </BrowserRouter>
     </div>
   );
 }
