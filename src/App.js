33407419d77aaa9eb67235c6e7a93683d7509aae
Leticia Lumi Nagao <leticia-nagao@hotmail.com>
2022-06-04 22:58:54 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Components/Header'; 
 import Footer from './Components/Footer';
 import Home from './Components/Home';
+import Login from './Components/Login/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
