9570630ee2cda478dd99ba36c92d23bcb948007e
letnagao <leticia-nagao@hotmail.com>
2022-06-13 19:58:37 -0300

    added head and footer

--- src/Components/Login/LoginPasswordReset.js
+++ src/Components/Login/LoginPasswordReset.js
@@ -6,6 +6,7 @@ import useFetch from '../../Hooks/useFetch';
 import { PASSWORD_RESET } from '../../Api';
 import Error from '../Helper/Error';
 import { useNavigate } from 'react-router-dom';
+import Head from '../Helper/Head';
 
 const LoginPasswordReset = () => {
   const[login, setLogin] = React.useState('');
@@ -37,6 +38,7 @@ const LoginPasswordReset = () => {
 
   return <div>
     <h1 className="title">Resete a Senha </h1>
+    <Head title="Resete a senha" />
     <form onSubmit={handleSubmit}>
       <Input label="Nova Senha" type="password" name="password" {...password}/>
       {loading ? (
